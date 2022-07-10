f9c878ef15e8cb823b438b87a2f5c4f98d532d32
yamhochi <yam.hoechee@gmail.com>
2022-07-11 00:35:56 +1000

    update apollo client

--- helpers/apollo-client.js
+++ helpers/apollo-client.js
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  // uri: "http://localhost:3000/api/graphql",
+  uri: process.env.CONNECT_URI,
   cache: new InMemoryCache(),
 });
 
 export default client;
+
