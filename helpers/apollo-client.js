bd72adf4e29c40a92e216d42111415ab9377bc51
yamhochi <yam.hoechee@gmail.com>
2022-11-08 10:07:02 +1100

    update apollo client cache settings

--- helpers/apollo-client.js
+++ helpers/apollo-client.js
@@ -1,13 +1,27 @@
 import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from "@apollo/client";
 
+const defaultOptions = {
+  watchQuery: {
+    // fetchPolicy: "cache-and-network",
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    // fetchPolicy: "network-only",
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
+
 export function createApolloClient() {
   const client = new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: createHttpLink({
       uri: process.env.NEXT_PUBLIC_API_URL,
-      credentials: 'same-origin',
+      // credentials: 'same-origin',
     }),
     cache: new InMemoryCache(),
+    defaultOptions,
   });
 
   return client;
